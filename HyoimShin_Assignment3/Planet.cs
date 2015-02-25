eaf7847a98a860525b13cce25444079ffcd9e59c
Hyoim SHin <hi0217@gmail.com>
2015-02-24 23:58:19 -0500

    Create Planet class

--- HyoimShin_Assignment3/Planet.cs
+++ HyoimShin_Assignment3/Planet.cs
@@ -8,8 +8,6 @@ namespace HyoimShin_Assignment3
 {
     class Planet
     {
-        /// <summary>
-        /// </summary>
         private double _diameter;
         private double _mass;
         private int _moonCount;
@@ -17,5 +15,89 @@ namespace HyoimShin_Assignment3
         private double _orbitalPeriod;
         private int _ringCount;
         private int _rotationPeriod;
+
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;      // Read-Only
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                return this._mass;          // Read-Only
+            }
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;      // Read-Only
+            }
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public int RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this._name + "\n" + "Diameter : " + this._diameter + "Mass : " + this._mass;
+        }
     }
 }
