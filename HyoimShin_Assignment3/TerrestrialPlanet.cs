453f229d65e5a664d593b93d9baf0467e69e2742
Hyoim SHin <hi0217@gmail.com>
2015-02-25 00:26:51 -0500

    Add TerrestrialPlanet class

---
+++ HyoimShin_Assignment3/TerrestrialPlanet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyoimShin_Assignment3
+{
+    class TerrestrialPlanet : Planet, IHasMoons, IHabitable
+    {
+        // PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++++++++
+        private bool _oxygen;
+
+        
+        // CONSTRUCTORS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) : base (name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+
+        // PUBLLIC METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public bool HasMoons()
+        {
+            return (this.MoonCount < 0) ? true : false;
+        }
+
+        public bool Habitable()
+        {
+            return (this._oxygen == true) ? true : false;
+        }
+    }
+}
