d4b8bf21e9057da265ce9f6ec23f5e4deefcc518
Hyoim SHin <hi0217@gmail.com>
2015-02-25 00:02:12 -0500

    Add Interface

---
+++ HyoimShin_Assignment3/IHasMoons.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyoimShin_Assignment3
+{
+    interface IHasMoons
+    {
+        bool HasMoons();
+    }
+}
