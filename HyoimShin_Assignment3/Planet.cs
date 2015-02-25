2101731e10a9d254751152caf390ac3f2b0a473e
Hyoim SHin <hi0217@gmail.com>
2015-02-24 23:39:45 -0500

    Create base VS C# Project

---
+++ HyoimShin_Assignment3/Planet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyoimShin_Assignment3
+{
+    class Planet
+    {
+        /// <summary>
+        /// </summary>
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private int _rotationPeriod;
+    }
+}
